b3a44ebccade409a2153dff188e7ceb9f980aaac
dariakrtn <dariakorotinawork@yandex.ru>
2023-04-11 22:41:25 +0300

    test version

---
+++ Assets/RawTexture.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+public class  RawTexture : MonoBehaviour
+{
+    
+    public RawImage m_RawImage;
+    //Select a Texture in the Inspector to change to
+    public Texture m_Texture;
+
+    void Start()
+    {
+        //Fetch the RawImage component from the GameObject
+        m_RawImage = GetComponent<RawImage>();
+        //Change the Texture to be the one you define in the Inspector
+        m_RawImage.texture = m_Texture;
+    }
+}
+
